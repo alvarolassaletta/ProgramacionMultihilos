dca15df46a56248d762c592905221195ed8f1beb
alvaro <alvarolassaletta@gmail.com>
2025-11-30 13:48:37 +0100

    docs: update enunciado

--- src/main/java/Ejercicio07Beta/MainBeta.java
+++ src/main/java/Ejercicio07Beta/MainBeta.java
@@ -4,8 +4,11 @@ public class MainBeta {
     public static void main(String[] args) {
 
         /**Otra forma de hacerlo en la que, en vez de implementar Runnable  directamente
-         *  en una clase, se usa una funcion lambda
+         *  en una clase, se usa una funcion lambda. Esto es posible porque Runnable es una interfaz
+         *  funcional(interfaces con un solo metodo abstracto -- el metodo run() en este caso--) y las
+         *  interfaces funcionales permiten esta implementación mediante lambda
          * */
+
         Thread thread1 = new Thread(()-> {
             System.out.println(Thread.currentThread().getName());
         });
